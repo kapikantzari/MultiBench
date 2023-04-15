b0933d8ee76dfa52a07a027aae90e6c9521f4db5
Yun Cheng <yuncheng@mars.multicomp.cs.cmu.edu>
2023-04-14 21:09:12 -0400

    fix unit tests

--- fusions/common_fusions.py
+++ fusions/common_fusions.py
@@ -404,3 +404,70 @@ class NLgate(torch.nn.Module):
         matmulled = torch.matmul(qin, kin)
         finalout = torch.matmul(self.softmax(matmulled), vin)
         return torch.flatten(qin + finalout, 1)
+
+
+class EarlyFusionTransformer(nn.Module):
+    """Implements a Transformer with Early Fusion."""
+    
+    embed_dim = 9
+
+    def __init__(self, n_features):
+        """Initialize EarlyFusionTransformer Object.
+
+        Args:
+            n_features (int): Number of features in input.
+        
+        """
+        super().__init__()
+
+        self.conv = nn.Conv1d(n_features, self.embed_dim,
+                              kernel_size=1, padding=0, bias=False)
+        layer = nn.TransformerEncoderLayer(d_model=self.embed_dim, nhead=3)
+        self.transformer = nn.TransformerEncoder(layer, num_layers=3)
+        self.linear = nn.Linear(self.embed_dim, 1)
+
+    def forward(self, x):
+        """Apply EarlyFusion with a Transformer Encoder to input.
+
+        Args:
+            x (torch.Tensor): Input Tensor
+
+        Returns:
+            torch.Tensor: Layer Output
+        """
+        x = self.conv(x.permute([0, 2, 1]))
+        x = x.permute([2, 0, 1])
+        x = self.transformer(x)[-1]
+        return self.linear(x)
+
+
+class LateFusionTransformer(nn.Module):
+    """Implements a Transformer with Late Fusion."""
+    
+    def __init__(self, embed_dim=9):
+        """Initialize LateFusionTransformer Layer.
+
+        Args:
+            embed_dim (int, optional): Size of embedding layer. Defaults to 9.
+        """
+        super().__init__()
+        self.embed_dim = embed_dim
+
+        self.conv = nn.Conv1d(
+            1, self.embed_dim, kernel_size=1, padding=0, bias=False)
+        layer = nn.TransformerEncoderLayer(d_model=self.embed_dim, nhead=3)
+        self.transformer = nn.TransformerEncoder(layer, num_layers=3)
+
+    def forward(self, x):
+        """Apply LateFusionTransformer Layer to input.
+
+        Args:
+            x (torch.Tensor): Layer input
+
+        Returns:
+            torch.Tensor: Layer output
+        """
+        x = self.conv(x.view(x.size(0), 1, -1))
+        x = x.permute(2, 0, 1)
+        x = self.transformer(x)[-1]
+        return x
